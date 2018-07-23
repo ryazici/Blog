5ff19e325b5da1a883b0dfc97f31e4b0c14059fc
ryazici <rdvnzc@gmail.com>
2018-07-23 23:59:23 +0300

    Irrelevant files removed

--- src/main/java/com/yazici/repository/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.yazici.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.yazici.entity.Room;
-
-public interface RoomRepository extends CrudRepository<Room, Long> {
-
-}
